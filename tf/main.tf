a86f626af724fc0b2243ec74ac40ae19f9517dc1
VinceDeslo <vincent.desloover.dev@gmail.com>
2022-11-22 16:20:56 -0500

    fix deprecated field.

--- tf/main.tf
+++ tf/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {}
-resource "aws_db_instance" "eda-fun" {
+resource "aws_db_instance" "edafun" {
   allocated_storage    = 10
   engine               = "postgres"
   engine_version       = "14.1"
   instance_class       = "db.t4g.micro"
-  name                 = "eda-fun"
+  db_name              = "edafun"
   username             = "eda"
   password             = "admin"
   parameter_group_name = "test-instance-postgres-14.1"
