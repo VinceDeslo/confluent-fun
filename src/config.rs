c9659b611a91b8af9db28fe299c96fc2aeb4c5d0
VinceDeslo <vincent.desloover.dev@gmail.com>
2022-11-23 10:57:09 -0500

    Update deps and config.

--- src/config.rs
+++ src/config.rs
@@ -2,8 +2,10 @@ use dotenv::dotenv;
 
 #[derive(Debug)]
 pub struct Config {
-    aws_api_key: String,
-    aws_api_secret: String,
+    aws_access_key_id: String,
+    aws_secret_access_key: String,
+    aws_region: String,
+    aws_rds_connection: String,
     confluent_bootstrap_server: String,
     confluent_api_key: String,
     confluent_api_secret: String,
@@ -12,10 +14,14 @@ pub struct Config {
 pub fn load_config() -> Config {
     dotenv().ok();
 
-    let aws_api_key = std::env::var("AWS_API_KEY")
-        .expect("AWS_API_KEY must be set."); 
-    let aws_api_secret = std::env::var("AWS_API_SECRET")
-        .expect("AWS_API_SECRET must be set."); 
+    let aws_access_key_id = std::env::var("AWS_ACCESS_KEY_ID")
+        .expect("AWS_ACCESS_KEY_ID must be set."); 
+    let aws_secret_access_key = std::env::var("AWS_SECRET_ACCESS_KEY")
+        .expect("AWS_SECRET_ACCESS_KEY must be set."); 
+    let aws_region = std::env::var("AWS_REGION")
+        .expect("AWS_REGION must be set.");
+    let aws_rds_connection = std::env::var("AWS_RDS_CONNECTION")
+        .expect("AWS_RDS_CONNECTION must be set.");
     let confluent_bootstrap_server = std::env::var("CONFLUENT_BOOTSTRAP_SERVER")
         .expect("CONFLUENT_BOOTSTRAP_SERVER must be set."); 
     let confluent_api_key = std::env::var("CONFLUENT_API_KEY")
@@ -24,8 +30,10 @@ pub fn load_config() -> Config {
         .expect("CONFLUENT_API_SECRET must be set."); 
 
     Config {
-        aws_api_key,
-        aws_api_secret,
+        aws_access_key_id,
+        aws_secret_access_key,
+        aws_region,
+        aws_rds_connection,
         confluent_bootstrap_server,
         confluent_api_key,
         confluent_api_secret,
