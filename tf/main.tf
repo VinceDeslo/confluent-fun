aa2a757f5bdf33ad403070cd3bd0ee84ce6fc05e
VinceDeslo <vincent.desloover.dev@gmail.com>
2022-12-18 11:03:06 -0500

    modify db infra.

--- tf/main.tf
+++ tf/main.tf
@@ -2,43 +2,34 @@ provider "aws" {
   region = "ca-central-1"
 }
 
-resource "aws_rds_cluster" "eda-fun" {
-  cluster_identifier = "eda-fun-cluster"
-  engine             = "aurora-postgresql"
-  engine_version     = "14.0"
-  availability_zones = ["ca-central-1a", "ca-central-1b"]
-  backup_retention_period = 5
-  port = 5432
-  master_username = "admin"
-  master_password = "password"
-  skip_final_snapshot = true
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_db_instance" "eda-fun" {
+  identifier             = "eda-fun"
+  db_name = "edafun"
+  instance_class         = "db.t4g.micro"
+  allocated_storage      = 5
+  max_allocated_storage = 5
+  engine                 = "postgres"
+  engine_version         = "14"
+  skip_final_snapshot    = true
+  publicly_accessible    = true
   vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  username               = "edafunadmin"
+  password               = "password"
 }
 
 resource "aws_security_group" "rds_sg" {
   name        = "rds_sg"
   description = "Security group for RDS instances"
-  vpc_id      = aws_vpc.vpc.id
+  vpc_id      = "${data.aws_vpc.default.id}"
 
   ingress {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "eda-fun-vpc"
   }
 }
